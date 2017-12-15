9cc453f9fadce99919534e204d74b2e69ff2760d
maxiaojie <1103173210@qq.com>
2017-12-15 09:16:06 +0800

    20171215 09：15项目正常启动

--- tms-storage/tms-storage-service/src/main/java/com/helloword/service/StorageManageService.java
+++ tms-storage/tms-storage-service/src/main/java/com/helloword/service/StorageManageService.java
@@ -12,12 +12,19 @@ public interface StorageManageService {
      * @param num 入库数量
      * @param errorNum 损坏卡号集合
      */
-    void saveCard(Integer num,List<String> errorNum);
+    String saveCard(Integer num,List<String> errorNum);
 
     /**
      * 年票下发
      * @param num 下发数量
      */
     String takeCard(Integer num,String name);
+
+    /**
+     * 年票摧毀
+     * @param cardNum 作废卡号
+     */
+    void destoryCard(String cardNum);
+
 
 }
--- tms-storage/tms-storage-serviceimpl/src/main/java/com/helloword/service/impl/StorageManageServiceImpl.java
+++ tms-storage/tms-storage-serviceimpl/src/main/java/com/helloword/service/impl/StorageManageServiceImpl.java
@@ -1,8 +1,8 @@
 package com.helloword.service.impl;
 
-import com.github.pagehelper.PageHelper;
 import com.helloword.entity.Card;
 import com.helloword.entity.CardExample;
+import com.helloword.exception.CardException;
 import com.helloword.mapper.CardMapper;
 import com.helloword.service.StorageManageService;
 import org.apache.commons.lang3.StringUtils;
@@ -22,13 +22,16 @@ public class StorageManageServiceImpl implements StorageManageService {
     @Autowired
     private CardMapper cardMapper;
 
-    public void saveCard(Integer num,List<String> errorNum) {
+    public String saveCard(Integer num,List<String> errorNum) {
 
         List<String> numList = new ArrayList<String>();
         //从数据库中获取最后一条数据
         Card card = cardMapper.findLast();
 
+        String begin ;
+        String end;
         if(card == null) {
+            begin = "0000001";
             int n = 0;
             //0000001
             for(int i = 0;i<num;i++) {
@@ -53,6 +56,7 @@ public class StorageManageServiceImpl implements StorageManageService {
                 }
                 value = stringBuilder+value;
                 numList.add(value);
+                begin = value;
             }
         }
         //调用cardMapper中自定义多条插入
@@ -74,30 +78,57 @@ public class StorageManageServiceImpl implements StorageManageService {
                     Card card1 = (Card) cardMapper.selectByExample(cardExample);
                     //损坏卡
                     card1.setState("非正常");
+                    cardMapper.updateByPrimaryKey(card1);
                 }
 
             }
         }
-
+return null;
     }
 
     /**
      * 年卡下发
      * @param num 下发数量
-     * @param name 领卡人姓名
+     * @param shopName 售票点名称
      * @return 领卡 id段
      */
-    public String takeCard(Integer num,String name) {
+    public String takeCard(Integer num,String shopName) {
         //查询t_card下 takeBy 为null 的 order by id asc limit 1 的id
         Card card = cardMapper.getFirstNull();
         String beginId = card.getId().toString();
 
         String endId = beginId+(num - 1);
 
-        cardMapper.updateTakeBy(name,beginId,endId);
+        cardMapper.updateTakeBy(shopName,beginId,endId);
 
+        //下发卡号id段
         String round = beginId+"-"+endId;
+
+        //添加一条card_order订单信息
+
         return round;
+    }
+
+    public void destoryCard(String cardNum) {
+        CardExample cardExample = new CardExample();
+        cardExample.createCriteria().andCardNumEqualTo(cardNum);
+        List<Card> cardList = cardMapper.selectByExample(cardExample);
+
+        if(cardList == null) {
+            throw new CardException("该卡号不存在");
+        }
+
+        Card card = cardList.get(0);
+
+        if("非正常".equals(card.getState())) {
+            throw new CardException("该卡属于作废状态");
+        } else if(StringUtils.isNotEmpty(card.getState())) {
+            throw new CardException("操作异常,该卡不能作废");
+        }
+
+        card.setState("非正常");
+        cardMapper.updateByPrimaryKey(card);
+
     }
 
 
